9520b01e0d689e051ad90e8e03d1e504f9092995
Diego Dorta <diego.d@variscite.com>
2021-07-18 18:36:00 -0300

    NEW: Add first image detection example
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ tflite/detection/image_detection.py
@@ -0,0 +1,7 @@
+# Copyright 2021 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+def image_object_detection():
+    print("Object Detection")
+
+if __name__ == "__main__":
+    image_object_detection()
