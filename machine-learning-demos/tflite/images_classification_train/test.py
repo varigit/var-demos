a23413049ca619c86e038bfb35b5c6b5ab389143
Diego Dorta <diego.d@variscite.com>
2021-11-15 21:43:29 -0300

    Change test to run inference on all examples
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- machine-learning-demos/tflite/images_classification_train/test.py
+++ machine-learning-demos/tflite/images_classification_train/test.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Variscite LTD
 # SPDX-License-Identifier: BSD-3-Clause
 
+import glob
 import os
 
 import numpy as np
@@ -17,30 +18,44 @@ def convert_to_8b_grayscale(image_path, maxsize):
 	image.thumbnail(maxsize, PIL.Image.ANTIALIAS)
 	return np.asarray(image)
 
+def read_labels(labels_file_path):
+    with open(labels_file_path, 'r', encoding='utf-8') as f:
+        labels_list = [line.strip() for line in f.readlines()]
+    return labels_list
+
 if __name__ == "__main__":
     DATA_DIR = os.path.join(os.getcwd(), "data")
     TFLITE_MODEL_DIR = os.path.join(os.getcwd(), "model")
-    class_names = ['orange', 'banana']
+
+    class_names = read_labels(labels_file_path=f"{TFLITE_MODEL_DIR}/tflite.txt")
     maxsize = 50, 50
     maxsize_w, maxsize_h = maxsize
     reshape_size = (maxsize_w, maxsize_h, 1)
 
-    input_image = convert_to_8b_grayscale(f"{DATA_DIR}/orange.jpg", maxsize)
-    input_image = input_image.reshape(reshape_size)
+    images = [f for f in glob.glob(f"{DATA_DIR}/*.jpg")]
 
-    input_image = np.array(input_image, dtype=np.float32) / 255.0
-    input_image = np.expand_dims(input_image, axis=0)
+    for image in images:
+        input_image = convert_to_8b_grayscale(image, maxsize)
+        input_image = input_image.reshape(reshape_size)
 
-    interpreter = Interpreter(model_path=f"{TFLITE_MODEL_DIR}/classification.tflite")
-    interpreter.allocate_tensors()
+        input_image = np.array(input_image, dtype=np.float32) / 255.0
+        input_image = np.expand_dims(input_image, axis=0)
 
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+        interpreter = Interpreter(model_path=f"{TFLITE_MODEL_DIR}/tflite_model.tflite")
+        interpreter.allocate_tensors()
 
-    interpreter.set_tensor(input_details[0]["index"], input_image)
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        print(input_details[0]['dtype'])
 
-    interpreter.invoke()
-    result = interpreter.tensor(output_details[0]["index"])()[0]
+        interpreter.set_tensor(input_details[0]["index"], input_image)
 
-    prediction = np.argmax(result)
-    print(f"Predicted Image: {class_names[prediction]}\nConfidence: {result[prediction]}")
+        interpreter.invoke()
+        result = interpreter.tensor(output_details[0]["index"])()[0]
+
+        prediction = np.argmax(result)
+        print("===============================================================")
+        print(f"Image: {image}\n")
+        print(prediction)
+        print(f"Prediction: {class_names[prediction]}\n")
+        print(f"Confidence: {result[prediction]}\n\n")
