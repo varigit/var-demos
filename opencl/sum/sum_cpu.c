a2a113d346719fd98803dccea974b0364610b33c
Diego Dorta <diego.d@variscite.com>
2022-07-06 21:47:21 -0300

    Add sum array example
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ opencl/sum/sum_cpu.c
@@ -0,0 +1,68 @@
+/* Sum Array Example for CPU
+ *
+ * MIT License
+ * Copyright 2022 Variscite LTD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SMALL_SIZE 80
+#define ARRAY_LARGE_SIZE 600000
+
+int main(void)
+{
+	static volatile int a[ARRAY_SMALL_SIZE];
+	static volatile int b[ARRAY_SMALL_SIZE];
+
+	static int m[ARRAY_LARGE_SIZE];
+	static int n[ARRAY_LARGE_SIZE];
+
+	static volatile int z[ARRAY_LARGE_SIZE];
+
+	int i = 0;
+	clock_t start = 0;
+	clock_t end = 0;
+	double cpu_time = 0.0;
+
+	for (i = 0; i < ARRAY_SMALL_SIZE; i++) {
+		a[i] = 2 * i;
+		b[i] = 3 * i;
+	}
+
+	for (i = 0; i < ARRAY_LARGE_SIZE; i++) {
+		m[i] = a[i % ARRAY_SMALL_SIZE];
+		n[i] = b[i % ARRAY_SMALL_SIZE];
+	}
+
+	start = clock();
+	for (i = 0; i < ARRAY_LARGE_SIZE; i++) {
+		z[i] = m[i] + n[i];
+	}
+	end = clock();
+
+	cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("[Execution Time] >> %f seconds.\n", cpu_time);
+
+	return EXIT_SUCCESS;
+}
