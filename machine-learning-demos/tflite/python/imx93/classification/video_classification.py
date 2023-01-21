8db243a4977029083cd4aea2d88041300ca3a96c
Diego Dorta <diego.d@variscite.com>
2023-01-21 12:47:25 -0300

    imx93: classification: remove framerate function
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- machine-learning-demos/tflite/python/imx93/classification/video_classification.py
+++ machine-learning-demos/tflite/python/imx93/classification/video_classification.py
@@ -7,8 +7,8 @@ import numpy as np
 import ethosu.interpreter as ethosu
 
 from helper.config import TITLE
-from helper.opencv import put_info_on_frame, put_fps_on_frame
-from helper.utils import load_labels, Timer, Framerate
+from helper.opencv import put_info_on_frame
+from helper.utils import load_labels, Timer
 
 # Properly pipeline for the i.MX 93
 # gst-launch-1.0 filesrc location=/home/root/Big_Buck_Bunny_720_10s_1MB.mp4 ! \
@@ -37,34 +37,31 @@ def video_classification(args):
     _, height, width, _ = input_details[0]['shape']
 
     video_capture = open_video_capture(args)
-    framerate = Framerate()
     while video_capture.isOpened():
-        with framerate.fpsit():
-            check, frame = video_capture.read()
-            if check is not True:
-                break
-            resized_frame = cv2.resize(frame, (width, height))
-            resized_frame = np.expand_dims(resized_frame, axis = 0)
+        check, frame = video_capture.read()
+        if check is not True:
+            break
+        resized_frame = cv2.resize(frame, (width, height))
+        resized_frame = np.expand_dims(resized_frame, axis = 0)
 
-            interpreter.set_input(input_details[0]['index'], resized_frame)
-            timer = Timer()
-            with timer.timeit():
-                interpreter.invoke()
+        interpreter.set_input(input_details[0]['index'], resized_frame)
+        timer = Timer()
+        with timer.timeit():
+            interpreter.invoke()
 
-            output = np.squeeze(interpreter.get_input(output_details[0]['index']))
+        output = np.squeeze(interpreter.get_input(output_details[0]['index']))
 
-            k = int(args['kresults'])
-            top_k = output.argsort()[-k:][::-1]
-            result = []
-            for i in top_k:
-                score = float(output[i] / 255.0)
-                result.append((i, score))
+        k = int(args['kresults'])
+        top_k = output.argsort()[-k:][::-1]
+        result = []
+        for i in top_k:
+            score = float(output[i] / 255.0)
+            result.append((i, score))
 
-            frame = put_info_on_frame(frame, result, labels,
-                                      timer.time, args['model'], args['video'])
-            frame = put_fps_on_frame(frame, framerate.fps)
-            cv2.imshow(TITLE, frame)
-            cv2.waitKey(1)
+        frame = put_info_on_frame(frame, result, labels,
+                                  timer.time, args['model'], args['video'])
+        cv2.imshow(TITLE, frame)
+        cv2.waitKey(1)
 
     video_capture.release()
     cv2.destroyAllWindows()
