598bd09f11513046d89ee4b9621c7b718dd0d0f1
Diego Dorta <diego.d@variscite.com>
2023-05-27 21:06:03 -0300

    vscode-demos: Improve code readability in hello-world.c
    1. Add copyright notice.
    2. Add detailed comments explaining the code.
    3. Change main function parameters to void, indicating it takes no arguments.
    4. Replace return value with EXIT_SUCCESS for better code readability.
    
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- vscode-demos/hello-world/hello-world.c
+++ vscode-demos/hello-world/hello-world.c
@@ -1,6 +1,30 @@
+/* Copyright 2023 Variscite Ltd */
+
+/* Include the standard input/output library. This library provides us with
+ * functions like printf() and scanf().
+ */
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+/* Include the standard library. This library provides us with functions like
+ * exit() and malloc(). Here, it provides the EXIT_SUCCESS macro.
+ */
+#include <stdlib.h>
+
+/* Main function. The operating system runs this function when the program
+ * starts. The void in the parentheses means this function takes no arguments.
+ */
+int main(void)
+{
+    /* Call the printf function from the stdio library to print the string
+     * "Hello, World!" to the console. The '\n' is a special character that
+     * represents a newline, so the cursor will move to the next line after
+     * printing the string.
+     */
     printf("Hello, World!\n");
-    return 0;
+
+    /* Return the EXIT_SUCCESS macro (which is typically defined to be 0) to
+     * the operating system, indicating that the program has successfully
+     * completed. This is a convention in C programming.
+     */
+    return EXIT_SUCCESS;
 }
